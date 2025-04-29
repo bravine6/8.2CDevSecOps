7c1786b26cc83f2099d29518192f4c0ca9f6aed9
Bravine Cheruiyot <ropbravine@gmail.com>
2025-04-30 09:20:27 +1000

    Add email notifications with attachLog to test & audit stages

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -15,6 +15,20 @@ pipeline {
             steps {                
                 sh 'npm test || true' // Allow pipeline to continue despite test failures
             }
+            post {
+                success {
+                    mail to: "mickybravine@gmail.com",
+                    subject: "Run Tests Stage: SUCCESS",
+                    body: "Run Tests has been successfully finished."
+                    attachLog: true
+                }
+                failure {
+                    mail to: "mickybravine@gmail.com",
+                    subject: "Run Tests Stage: FAILED",
+                    body: "Run Tests has failed."
+                    attachLog: true
+                }
+            }
         }
         stage('Generate Coverage Report') {
             steps {
@@ -25,6 +39,20 @@ pipeline {
         stage('NPM Audit (Security Scan)') {
             steps {                
                 sh 'npm audit || true' // This will show known CVEs in the output
+            }
+            post {
+                success {
+                    mail to: "mickybravine@gmail.com",
+                    subject: "Security Scan: SUCCESS",
+                    body: "Security scan has been successfully finished"
+                    attachLog: true
+                }
+                failure {
+                    mail to: "mickybravine@gmail.com",
+                    subject: "Security scan Stage: FAILED",
+                    body: "NPM Audit has failed."
+                    attachLog: true
+                }
             }
         }
     }
